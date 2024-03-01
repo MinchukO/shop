0297dc6cde9fb5eb1bd566c1aac1fc3d04304ff7
Minchuk Olga <olyamarchenko22@gmail.com>
2024-03-01 13:40:11 +0100

    github page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,8 +33,8 @@ function Login() {
     <section className="h-screen grid place-items-center">
       <Form method="post" className="card w-96  p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
         <h4 className="text-center text-3xl font-bold">Login</h4>
-        <FormInput type="email" label="email" name="identifier" />
-        <FormInput type="password" label="password" name="password" />
+        <FormInput type="email" label="email" name="identifier" defaultValue="ol343@gmail.com" />
+        <FormInput type="password" label="password" name="password" defaultValue="34568765434" />
         <div className="mt-4">
           <SubmitBtn text="login" />
         </div>
